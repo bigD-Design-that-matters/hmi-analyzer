73865daa8b91334159b4684dddf55dc062e4b308
bigD <info@bigd.es>
2026-04-07 14:12:52 +0200

    Update app_light.py

--- app_light.py
+++ app_light.py
@@ -137,7 +137,7 @@ uploaded_file = st.file_uploader(
 st.markdown(
     """
     <div style="
-        background-color:rgba(153,167,197,0.1);
+        background-color:rgba(146,461,191,0.1);
         border-left:4px solid #1e88e5;
         padding:14px 16px;
         margin-top:10px;
